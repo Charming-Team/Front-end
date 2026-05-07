0e53ee446ff004e3900fa6650ae2c9fc664f251d
Dabeen Hwang <dabin12648@gmail.com>
2026-05-07 09:33:28 +0900

    TailwindCSS v4 setting

--- s_map/src/components/HelloWorld.vue
+++ s_map/src/components/HelloWorld.vue
@@ -9,4 +9,13 @@ const count = ref(0)
 
 <template>
   <button class="btn btn-primary">Bootstrap OK</button>
+  <div class="min-h-screen bg-slate-100 p-10">
+    <h1 class="text-3xl font-bold text-blue-600">
+      Tailwind 연결 성공
+    </h1>
+
+    <button class="mt-4 rounded-lg bg-black px-4 py-2 text-white">
+      테스트 버튼
+    </button>
+  </div>
 </template>
